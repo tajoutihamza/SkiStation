6d9a4c58cd5cb8ba68411e1ea36ba55e5edf5fb0
tajoutihamza <tajouti.hamza@gmail.com>
2023-10-16 10:35:30 +0100

    add keywords for search to skier

--- src/main/java/Hamza/Taoujouti/skistation/repositories/ISkieurRepository.java
+++ src/main/java/Hamza/Taoujouti/skistation/repositories/ISkieurRepository.java
@@ -1,7 +1,17 @@
 package Hamza.Taoujouti.skistation.repositories;
 
 import Hamza.Taoujouti.skistation.entities.Skieur;
+import Hamza.Taoujouti.skistation.entities.enums.Couleur;
+import Hamza.Taoujouti.skistation.entities.enums.Support;
+import Hamza.Taoujouti.skistation.entities.enums.TypeAbonnement;
+import Hamza.Taoujouti.skistation.entities.enums.TypeCours;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ISkieurRepository extends CrudRepository<Skieur,Long> {
+    List<Skieur> findSkieurByAbonnementTypeAbonnement(TypeAbonnement abonnement_TypeAbonnement);
+    int countByAbonnementTypeAbonnementAndPistesCouleurAndInscriptionsCoursTypeCoursAndInscriptionsCoursSupport(TypeAbonnement abonnement_typeAbonnement, Couleur pistes_couleur, TypeCours inscriptions_cours_typeCours, Support inscriptions_cours_support);
+    List<Skieur> findByDateNaissanceBetween(LocalDate dateNaissance, LocalDate dateNaissance2);
 }
